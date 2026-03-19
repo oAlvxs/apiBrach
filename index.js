cf0c2f27ca9dcf71ff7bdd6e8d4a391316cee190
oAlvxs <biel.3546@outlook.com>
2026-03-19 20:23:06 -0300

    Etapa 1

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+
+
+app.get("/", (req, res) => {
+  res.send("Olá Troquei de brach");
+});
+
+
+app.listen(3005, () => {
+  console.log("Servidor on");
+});
